e793eb1d93eb08eb9a85d4df9bf803fb60d9fcb3
harikakoye <harikakoyi@gmail.com>
2017-10-13 10:12:28 +0000

    Add files via upload

---
+++ untitled3/sample3/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm, inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Customer, Address
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        exclude = ()
+class AddressForm(ModelForm):
+    class Meta:
+        model = Address
+        exclude = ()
---
+++ untitled3/sample3/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.test import Client
+from .forms import *
+class Setup_Class(TestCase):
+    def setUp(self):
+        self.customer = Customer.objects.create(first_name="user", last_name="customer",mobile_number=9990002221,phone_number=8885556666,email_id ="user@mp.com")
+        self.address = Address.objects.create(customer='self.customer',address1='h.no-212', address2='2nd lane',city='secunderabad', state='telangana', landmark='secondlane',pincode=500024)
+class Customer_Form_Test(TestCase):
+    def test_CustomerForm_valid(self):
+        form = CustomerForm(data={'first_name':"user", 'last_name': 'customer',  'mobile_number':9990002221,'phone_number':8885556666,'email_id' :"user@mp.com"})
+        self.assertTrue(form.is_valid())
+    def test_CustomerForm_invalid(self):
+        form = CustomerForm(data={'first_name': "las%1", 'last_name': "Cus tomer",'mobile_number':'9999sdfd','phone_number':'999 888','email_id' :""})
+        self.assertFalse(form.is_valid())
+class Address_Form_Test(TestCase):
+    def test_AddressForm_valid(self):
+        form = AddressForm(data={'customer':'self.customer','address1': 'h.no-212', 'address2': '2nd lane', 'city': 'secunderabad','state': 'telangana', 'landmark': 'secondlane', 'pincode': 500024})
+        self.assertTrue(form.is_valid())
+    def test_AddressForm_invalid(self):
+        form = AddressForm(data={'customer':'','address1': '', 'address2': '', 'city': 'secund@rabad2', 'state': 'telan&ana1','landmark': '', 'pincode': ''})
+        self.assertFalse(form.is_valid())
+
+
